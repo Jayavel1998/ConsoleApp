e1c1d7fb98742a41bb721b84880c2a0cff13f1e9
Jayavel1998 <jayavellaptop@gmail.com>
2024-10-02 22:04:00 +0530

    Initial commit

---
+++ JayaConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JayaConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /*
+            string name;
+            name = "Jayavel";
+            Console.writeLine("Your name");
+          name= Console.ReadLine();
+
+            Console.WriteLine(name);
+           */
+
+            int age;
+            age = 10;
+            Console.WriteLine(age+"  "+"123");
+            Console.ReadLine();
+
+            string name;
+            Console.WriteLine("Enter  :");
+            name = Console.ReadLine();
+
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
